b991fd6b3eda035cd42e59d76a52f0e754ecadcd
sultan malik <sultan.malik@city.ac.uk>
2021-02-16 06:07:53 +0000

    Initial commit after REDUX problem

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'WTF Taekwondo Suit',
+            category: 'Suits',
+            image: '/images/p1.jpg',
+            price:120,
+            countInStock: 10,
+            brand: 'WTF',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Taekwondo Suit',
+            category: 'Suits',
+            image: '/images/p2.jpg',
+            price:100,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Nike Taekwondo Suit',
+            category: 'Suits',
+            image: '/images/p3.jpg',
+            price:220,
+            countInStock: 30,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Adidas Taekwondo Belts',
+            category: 'Belts',
+            image: '/images/p4.jpg',
+            price:70,
+            countInStock: 5,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Adidas Red Body Armour',
+            category: 'Body Armour',
+            image: '/images/p5.jpg',
+            price:80,
+            countInStock: 0,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '6',
+            name: 'Adidas Blue Body Armour',
+            category: 'Body Armour',
+            image: '/images/p6.jpg',
+            price:80,
+            countInStock: 12,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+    ]
+}
+
+export default data;
