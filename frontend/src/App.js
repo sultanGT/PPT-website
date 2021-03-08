08691e4cd87f3b9e74f6fe111247d4ddbd6e32d0
sultan malik <sultan.malik@city.ac.uk>
2021-03-08 18:02:30 +0000

    place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
@@ -62,6 +63,7 @@ const signoutHandler = () => {
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen} exact></Route>
           <Route path="/payment" component={PaymentMethodScreen} exact></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen} exact></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
