eb98430b1ca1037e2d548e8685e4a26c9b11eda4
sultan malik <sultan.malik@city.ac.uk>
2021-03-22 09:56:48 +0000

    git push heroku

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
 
-
 dotenv.config();
 
 const app = express();
@@ -20,32 +19,26 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/PPT-website', {
   useCreateIndex: true,
 });
 
-
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-app.get('/api/config/paypal', (req, res) =>{
+app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) => 
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
-
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
-//dfd
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
