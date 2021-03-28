05046c7e79a61893033be78921adb5226b182909
sultan malik <sultan.malik@city.ac.uk>
2021-03-28 15:18:16 +0100

    npm updates

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
-import { isAdmin, isAuth } from '../utils.js';
+import { isAdmin, isAuth, mailgun, payOrderEmailTemplate } from '../utils.js';
 
 const orderRouter = express.Router();
 orderRouter.get(
